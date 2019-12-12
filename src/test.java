e7944cab821a64775c262f288feb5cd854eab9e1
Zigior <1103051218@qq.com>
2019-12-12 09:24:41 +0800

    BranchA test push

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args){
         System.out.println("hello git!");
         System.out.println("push to git!");
+        System.out.println("TestBranchA push to git!");
     }
 }
